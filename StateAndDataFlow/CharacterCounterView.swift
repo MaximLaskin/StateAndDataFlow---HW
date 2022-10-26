120f9e07d982dffc319e58b237f71622159b28ca
MaximLaskin <maxlaskin@mail.ru>
2022-10-26 22:07:35 +0500

    Initial Commit

---
+++ StateAndDataFlow/CharacterCounterView.swift
@@ -0,0 +1,29 @@
+//
+//  CharacterCounterView.swift
+//  StateAndDataFlow
+//
+//  Created by Maxim on 26.10.2022.
+//
+
+import SwiftUI
+
+struct CharacterCounterView: View {
+    @Binding var character: String
+    let successColor = Color.green // разумно сюда вынести или захардкодить в 17 строку?
+    let unsuccessColor = Color.red
+
+    var body: some View {
+        Text("\(character.count)")
+            .foregroundColor(character.count > 2 ? successColor : unsuccessColor)
+        /*
+         не захотел выносить логику из представления. Кажется она тут изящно вяжется на 1 свойстве character. Cразу и цвет и count))
+         */
+    }
+}
+
+struct CharacterCounterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterCounterView(character: .constant("Maxim"))
+    }
+}
+
---
+++ StateAndDataFlow/RegisterView.swift
@@ -0,0 +1,41 @@
+//
+//  RegisterView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 31.03.2021.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @EnvironmentObject var user: UserManager
+    @State private var name = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                TextField("Enter your name", text: $name)
+                    .multilineTextAlignment(.center)
+                CharacterCounterView(character: $name)
+                    .padding()
+            }
+            Button(action: registerUser) {
+                Image(systemName: "checkmark.circle")
+                Text("Ok")
+            }
+        }
+    }
+    
+    private func registerUser() {
+        if !name.isEmpty {
+            user.name = name
+            user.isRegister.toggle()
+        }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
